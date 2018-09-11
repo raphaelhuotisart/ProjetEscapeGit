7d8475f3f249656874e8228f032657172b313e05
5odrek <a.tab@student.isartdigital.com>
2018-09-10 22:36:45 -0400

    OriginalPush

---
+++ ProjectEscape/Assets/Scripts/KillingPlane.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KillingPlane : MonoBehaviour {
+
+
+   void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+      
+        if (hit.gameObject.tag == "KillingPlane")
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
